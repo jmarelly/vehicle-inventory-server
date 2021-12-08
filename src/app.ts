cdf918dd52f021391763c5a76281c2b9b651eccd
jmarelly <jonathanmarelly@gmail.com>
2021-12-08 18:58:36 +0200

    fixed cors

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import api from "./routes/api";
 
 const app = express();
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 
@@ -16,7 +17,7 @@ app.use((req, res, next) => {
 })
 
 app.use(express.json());
-app.use(cors());
+
 
 app.use(api);
 
